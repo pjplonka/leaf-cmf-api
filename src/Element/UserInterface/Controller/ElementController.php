14896527792faa1a98ca79d68bc2bda89d2e50ba
pjplonka <pjplonka@gmail.com>
2024-01-14 20:17:33 +0100

    Add ValidationFailedExceptionSubscriber to return simplified array with errors.

--- src/Element/UserInterface/Controller/ElementController.php
+++ src/Element/UserInterface/Controller/ElementController.php
@@ -1,9 +1,10 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace App\Element\UserInterface\Controller;
 
 use Leaf\Core\Application\Common\Command\CommandBus;
-use Leaf\Core\Application\Common\Exception\ValidationFailedException;
 use Leaf\Core\Application\Common\FieldDTO;
 use Leaf\Core\Application\CreateElement\CreateElementCommand;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -21,20 +22,11 @@ class ElementController
         $command = new CreateElementCommand(
             'products',
             Uuid::v4(),
-            new FieldDTO('name', 'John'),
+            new FieldDTO('name2', 'John'),
             new FieldDTO('delivered_at', '2022-05-01'),
         );
 
-        try {
-            $commandBus->handle($command);
-        } catch (ValidationFailedException $e) {
-            $errors = [];
-            foreach ($e->violations as $violation) {
-                $errors[str_replace(['[', ']'], '', $violation->getPropertyPath())][] = $violation->getMessage();
-            }
-
-            return new JsonResponse($errors);
-        }
+        $commandBus->handle($command);
 
         return new JsonResponse([], 201);
     }
